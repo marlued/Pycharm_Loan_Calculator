1fcace25b2b1a38c00120002b41f94a01456ede2
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-05-16 12:38:23 +0200

    loan_calculator_stage_three.py added user input for all alternatives

--- loan_calculator_stage_three.py
+++ loan_calculator_stage_three.py
@@ -1,6 +1,7 @@
 def monthly_payment():
     pass
 
+
 starting_string = """
 What do you want to calculate?
 type "n" for number of monthly payments,
@@ -13,4 +14,14 @@ user_input = input().strip()
 if user_input == "a":
     loan_principal = int(input('Enter the loan principal: '))
     number_of_periods = int(input('Enter the number of periods: '))
+    loan_interest = int('Enter the loan interest: ')
+
+if user_input == "p":
+    annuity_payment = int(input('Enter the annuity payment:'))
+    number_of_periods = int(input('Enter the number of periods: '))
+    loan_interest = int('Enter the loan interest: ')
+
+if user_input == "n":
+    loan_principal = int(input('Enter the loan principal: '))
+    monthly_payment = int(input('Enter the monthly payment: '))
     loan_interest = int('Enter the loan interest: ')
