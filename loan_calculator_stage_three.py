bd784107bc24c6c79f3902eb8bff3463acd9ab03
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-05-16 22:32:42 +0200

    function that calculates the monthly payment created and tested

--- loan_calculator_stage_three.py
+++ loan_calculator_stage_three.py
@@ -1,8 +1,8 @@
-from math import log, ceil
+from math import log, ceil, floor
 
 
-def monthly_payment():
-    pass
+def monthly_payment(loan, interest, periods):
+    return floor(loan * (interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
 
 
 def number_of_monthly_payments(loan, payment, interest):
@@ -32,6 +32,10 @@ if user_input == "a":
     loan_principal = int(input('Enter the loan principal: '))
     number_of_periods = int(input('Enter the number of periods: '))
     loan_interest = int(input('Enter the loan interest: '))
+
+    i = nominal_interest_rate(loan_interest)
+    pay_per_month = monthly_payment(loan_principal, i, number_of_periods)
+    print(f'Your monthly payment = {pay_per_month}!')
 
 if user_input == "p":
     annuity_payment = int(input('Enter the annuity payment:'))
