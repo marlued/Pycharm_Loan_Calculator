c03da3cfc706af65f59a62c14d9e1fb0b6e6741d
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-05-16 12:57:37 +0200

    working version - all alternatives can be selected. Functions do not have any functionallity.

--- loan_calculator_stage_three.py
+++ loan_calculator_stage_three.py
@@ -1,8 +1,10 @@
 def monthly_payment():
     pass
 
+
 def number_of_monthly_payments():
     pass
+
 
 def loan_principal():
     pass
@@ -12,22 +14,24 @@ starting_string = """
 What do you want to calculate?
 type "n" for number of monthly payments,
 type "a" for annuity monthly payment amount,
-type "p" for loan principal:
+type "p" for loan principal: \n
 """
+
+print(starting_string)
 
 user_input = input().strip()
 
 if user_input == "a":
     loan_principal = int(input('Enter the loan principal: '))
     number_of_periods = int(input('Enter the number of periods: '))
-    loan_interest = int('Enter the loan interest: ')
+    loan_interest = int(input('Enter the loan interest: '))
 
 if user_input == "p":
     annuity_payment = int(input('Enter the annuity payment:'))
     number_of_periods = int(input('Enter the number of periods: '))
-    loan_interest = int('Enter the loan interest: ')
+    loan_interest = int(input('Enter the loan interest: '))
 
 if user_input == "n":
     loan_principal = int(input('Enter the loan principal: '))
     monthly_payment = int(input('Enter the monthly payment: '))
-    loan_interest = int('Enter the loan interest: ')
+    loan_interest = int(input('Enter the loan interest: '))
