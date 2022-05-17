8b67738aea5dab97bd25310dbfbe88035bbcb3e4
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-05-17 05:20:37 +0200

    completed and tested function calculating the loan principal. Changed int to float on input where necessary

--- loan_calculator_stage_three.py
+++ loan_calculator_stage_three.py
@@ -1,16 +1,16 @@
 from math import log, ceil, floor
 
 
-def monthly_payment(loan, interest, periods):
-    return floor(loan * (interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
+def monthly_payment(credit, interest, periods):
+    return floor(credit * (interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
 
 
-def number_of_monthly_payments(loan, payment, interest):
-    return ceil(log((payment / (payment - interest * loan)), 1 + interest))
+def number_of_monthly_payments(credit, payment, interest):
+    return ceil(log((payment / (payment - interest * credit)), 1 + interest))
 
 
-def loan_principal():
-    pass
+def loan_principal(payment, i, periods):
+    return payment / ((i * (1 + i) ** periods) / ((1 + i) ** periods - 1))
 
 
 def nominal_interest_rate(interest):
@@ -29,23 +29,27 @@ print(starting_string)
 user_input = input().strip()
 
 if user_input == "a":
-    loan_principal = int(input('Enter the loan principal: '))
+    loan_principal = float(input('Enter the loan principal: '))
     number_of_periods = int(input('Enter the number of periods: '))
-    loan_interest = int(input('Enter the loan interest: '))
+    loan_interest = float(input('Enter the loan interest: '))
 
     i = nominal_interest_rate(loan_interest)
     pay_per_month = monthly_payment(loan_principal, i, number_of_periods)
     print(f'Your monthly payment = {pay_per_month}!')
 
 if user_input == "p":
-    annuity_payment = int(input('Enter the annuity payment:'))
+    annuity_payment = float(input('Enter the annuity payment: '))
     number_of_periods = int(input('Enter the number of periods: '))
-    loan_interest = int(input('Enter the loan interest: '))
+    loan_interest = float(input('Enter the loan interest: '))
+
+    i = nominal_interest_rate(loan_interest)
+    loan = round(loan_principal(annuity_payment, i, number_of_periods))
+    print(f'Your loan principal = {loan}!')
 
 if user_input == "n":
-    loan_principal = int(input('Enter the loan principal: '))
-    monthly_payment = int(input('Enter the monthly payment: '))
-    loan_interest = int(input('Enter the loan interest: '))
+    loan_principal = float(input('Enter the loan principal: '))
+    monthly_payment = float(input('Enter the monthly payment: '))
+    loan_interest = float(input('Enter the loan interest: '))
 
     i = nominal_interest_rate(loan_interest)
 
